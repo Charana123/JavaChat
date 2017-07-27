511871493590c56f43b6cf0c565de84aabeded0a
Charana <charananandasena@yahoo.com>
2017-07-27 16:02:35 +0100

    Swaped synchronising on DB calls for asynchronous completion handlers (callbacks) that update the UI after the DB call completes TODO - Finish GUI for ChatController

--- src/main/java/com/charana/chat_window/ChatController.java
+++ src/main/java/com/charana/chat_window/ChatController.java
@@ -1,54 +1,79 @@
 package com.charana.chat_window;
 
 import com.charana.database_server.user.User;
+import com.charana.login_window.BaseWindowController;
+import com.charana.login_window.utilities.database.DatabaseConnector;
+import com.charana.login_window.utilities.database.ServerConnector;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
-public class ChatController implements Initializable {
+public class ChatController extends BaseWindowController implements Initializable {
+
+    @FXML VBox contentContainer;
 
     @FXML ListView<UserSidebarButtonControl> recentContacts;
     ObservableList<UserSidebarButtonControl> recentContactsItemsView;
     ArrayList<UserSidebarButtonControl> userecentContactsItems;
-    User user;
+    private final ServerConnector serverConnector;
+    private final DatabaseConnector dbConnector;
+    private final Stage chatStage;
+    private User user;
 
-    private ChatController(){
 
+    private ChatController(Stage chatStage, User user, ServerConnector serverConnector){
+        super();
+        this.serverConnector = serverConnector;
+        serverConnector.setWarningDialogCallbacks(
+                (Void voidz) -> hideWarningDialog(),
+                (String warningHeader, String warningContent) -> showWarningDialog(warningHeader, warningContent));
+        chatStage.setOnCloseRequest(event -> serverConnector.disconnect());
+        this.dbConnector = new DatabaseConnector(serverConnector);
+        this.chatStage = chatStage;
+        this.user = user;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //Anything code prior to scene populating (attaching listeners, loading data from model)
         recentContactsItemsView = recentContacts.getItems();
-
-        //TODO::Make an SQL Query to the Server to get a list of User objects which are Friends of the currently logged individual
-        //TODO:: What is the currently logged in individuals email ? but alongside email we should have all his other data
-        //TODO:: Should be during login use Constructor DI of the Users User object into the chat controller, which will populate other UI
-        //TODO:: elements
-
         loadRecentContacts();
     }
 
-    //TODO:: Gets a list of friends (from the database). Creates UserSidebarButtonControls for each and populates view.
-    //TODO:: Each contact has his own "Friends" table. We can "SELECT * FROM Friends" the entire table.
-    void loadRecentContacts(){
-        //We have a embedded database. It can hold account and login data but doesn't work when multiple clients are launched from
-        //different machines
-        //Each account has an assosiated friends table.
-
-        //SO right now we need to swap "SQLite" for a server database like "H2"
+    void loadRecentContacts(){ //TODO:: Should this maybe load a different sub-view ?
+        dbConnector.getFriends(user.getEmail(), (Boolean success, List<User> friends) -> {
+            List<UserSidebarButtonControl> userSidebarButtonControls = friends.stream().map(friend -> new UserSidebarButtonControl(170, 50, friend)).collect(Collectors.toList());
+            recentContacts.getItems().addAll(userSidebarButtonControls);
+        });
     }
 
+    @FXML
+    void viewContacts(){
+        dbConnector.getFriends(user.getEmail(), (Boolean success, List<User> friends) -> {
+            Parent root = ContactsMainViewController.getInstance(user, friends);
+            swapContent(root);
+        });
+    }
+
+    private void swapContent(Parent root){
+        contentContainer.getChildren().removeAll(contentContainer.getChildren());
+        contentContainer.getChildren().add(root);
+    }
 
     void updateRecentContacts(User user){
         //Specification -
@@ -57,14 +82,14 @@ public class ChatController implements Initializable {
         //An ArrayList holds all users, when a user is clicked (most recent). This method is called (with a structure of a user)
         //The user is then found in the ArrayList removed and added again (to become to the most recent)
         //The observable list is then cleared and reset with the new ArrayList
-
     }
 
-    public static Stage chatWindow(){
+    public static Stage chatWindow(User user, ServerConnector serverConnector){
         FXMLLoader fxmlLoader = new FXMLLoader(ChatController.class.getResource("/views/chat_window/MainLoginView.fxml"));
         try {
-            Parent root = fxmlLoader.load();
             Stage chatStage = new Stage();
+            fxmlLoader.setController(new ChatController(chatStage, user, serverConnector));
+            Parent root = fxmlLoader.load();
             chatStage.setScene(new Scene(root));
             return chatStage;
         }
@@ -73,7 +98,6 @@ public class ChatController implements Initializable {
             return null;
         }
     }
-
 
 }
 
