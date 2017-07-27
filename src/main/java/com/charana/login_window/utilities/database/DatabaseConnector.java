95d0c2c0e236a0cd796030f624d4ff14b67f402d
Charana <charananandasena@yahoo.com>
2017-07-27 09:56:06 +0100

    Started production of ChatController GUI/DatabaseConnections TODO - Swap synchronising on DB calls for asynchronous completion handlers (callbacks) that update the UI after the DB calls complete.

--- src/main/java/com/charana/login_window/utilities/database/DatabaseConnector.java
+++ src/main/java/com/charana/login_window/utilities/database/DatabaseConnector.java
@@ -1,10 +1,12 @@
 package com.charana.login_window.utilities.database;
 
 import com.charana.database_server.user.User;
-import com.charana.server.message.database_message.database_command_messages.concrete_database_command_messages.AccountExistsMessage;
-import com.charana.server.message.database_message.database_command_messages.concrete_database_command_messages.CreateAccountMessage;
-import com.charana.server.message.database_message.database_command_messages.concrete_database_command_messages.LoginMessage;
-import com.charana.server.message.database_message.database_command_messages.concrete_database_command_messages.ResetPasswordMessage;
+import com.charana.server.message.Message;
+import com.charana.server.message.database_message.database_command_messages.concrete_database_command_messages.*;
+import com.charana.server.message.database_message.database_response_messages.concrete_database_response_messages.GetAccountResponseMessage;
+import com.charana.server.message.database_message.database_response_messages.concrete_database_response_messages.GetFriendsResponseMessage;
+
+import java.util.List;
 
 
 public class DatabaseConnector {
@@ -16,22 +18,37 @@ public class DatabaseConnector {
 
     public boolean login(String email, String password) {
         serverConnector.send(new LoginMessage(null, email, password));
-        return serverConnector.DequeueResponse();
+        return serverConnector.DequeueResponse().success;
     }
 
     public boolean accountExists(String email){
         serverConnector.send(new AccountExistsMessage(null, email));
-        return serverConnector.DequeueResponse();
+        return serverConnector.DequeueResponse().success;
     }
 
     public boolean resetPassword(String email, String newPassword){
         serverConnector.send(new ResetPasswordMessage(null, email, newPassword));
-        return serverConnector.DequeueResponse();
+        return serverConnector.DequeueResponse().success;
     }
 
     public boolean createAccount(User user){
         serverConnector.send(new CreateAccountMessage(null, user));
-        return serverConnector.DequeueResponse();
+        return serverConnector.DequeueResponse().success;
     }
+
+    public User getAccount(String email){
+        serverConnector.send(new GetAccountMessage(null, email));
+        return ((GetAccountResponseMessage) serverConnector.DequeueResponse()).user;
+    }
+
+    public List<User> getFriends(String email){
+        serverConnector.send(new GetFriendsMessage(null, email));
+        return ((GetFriendsResponseMessage) serverConnector.DequeueResponse()).friends;
+    }
+
+    //TODO:: Set a onCompletionHandler, which is passed to the db access function
+    //TODO:: The db access function spins a thread that continously keeps trying to send the message to the server until it succeds
+    //TODO:: At which point it dequeues the response (if it takes longer than 1 second) it assumed the message wasn't sent back and retries sending
+    //TODO:: The call back is then called to update the UI in some way to notify the user
 
 }
